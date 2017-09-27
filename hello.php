6e754c0f3d6aaf6936502b9d1c11e593369ce59b
Stephanie Manzano <loveforlilypads@users.noreply.github.com>
2017-09-26 20:50:51 -0700

    Create hello.php
    Wk1 Assignment

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!-- 
+	
+  Author: Stephanie Manzano
+	Course: CS85
+	Instructor:  seno_vicky@smc.edu
+
+-->
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
